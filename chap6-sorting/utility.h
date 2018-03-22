2305e4a0d5d9e44fd9bb7025012bd954e70ebb96
ahljjun <liu.ljjun@gmail.com>
2018-03-22 22:43:39 +0800

    refine random

--- chap6-sorting/utility.h
+++ chap6-sorting/utility.h
@@ -6,13 +6,12 @@
 #include <iostream>
 
 template <size_t N>
-void generateRandomData(std::array<int, N>& arr)
+void generateRandomDataSet(std::array<int, N>& arr)
 {
 	for (int i=0; i < N; i++)
-		arr[i] = 1000 * rand(); //(1.0 * rand()/RADN_MAX);
+		arr[i] = 1000 * (1.0 * rand()/RAND_MAX);
 
 #ifdef DEBUG
-	//for (auto it = arr.begin(); it != arr.end(); it++)
 	for (auto v : arr)
 		std::cout<<v<<", ";
 
