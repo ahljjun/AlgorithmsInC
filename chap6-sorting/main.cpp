760559fe22d8eef76425945300e31f12d47bbd0f
ahljjun <liu.ljjun@gmail.com>
2018-03-23 10:30:07 +0800

    selection sort

--- chap6-sorting/main.cpp
+++ chap6-sorting/main.cpp
@@ -4,5 +4,11 @@
 int main()
 {
 	std::array<int, 10> arr;
-	generateRandomData(arr);
+	generateRandomDataSet(arr);
+
+	selection_sort(arr);
+
+	std::cout<<"after selection sort: "<<"\n";
+
+	std::copy(arr.begin(), arr.end(), std::ostream_iterator<int>(std::cout, ", "));
 }
--- chap6-sorting/utility.h
+++ chap6-sorting/utility.h
@@ -4,14 +4,16 @@
 #include <cstdlib>
 #include <array>
 #include <iostream>
+#include <algorithm>
 
 template <size_t N>
 void generateRandomDataSet(std::array<int, N>& arr)
 {
 	for (int i=0; i < N; i++)
-		arr[i] = 1000 * (1.0 * rand()/RAND_MAX);
+		arr[i] = 1000 * (1.0 * rand()/(double)RAND_MAX);
 
 #ifdef DEBUG
+	std::cout<<"random data set: "<<std::endl;
 	for (auto v : arr)
 		std::cout<<v<<", ";
 
@@ -19,6 +21,18 @@ void generateRandomDataSet(std::array<int, N>& arr)
 #endif
 }
 
-
+template<class T, size_t N>
+void selection_sort(std::array<T, N>& arr)
+{
+	for(int i=0; i < N; i++){
+		int min = i;
+		for(int j=i+1; j < N; j++){
+			if ( arr[j] < arr[i] ){
+				min = j;
+			}
+		}
+		std::swap(arr[i], arr[min]);
+	}
+}
 
 #endif
